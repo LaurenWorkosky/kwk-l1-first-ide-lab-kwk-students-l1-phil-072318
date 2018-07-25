94f3a7454c03af15b40f9995999563fe95747612
LaurenWorkosky <19lworkosky@student.dasd.org>
2018-07-25 13:50:09 +0000

    automatically backed up by learn

--- guess.rb
+++ guess.rb
@@ -8,16 +8,15 @@ def guesser
     if guess < 5
     puts "sorry, too low! input a number from 1 to 10"
     guess = gets.chomp.to_i
-    time = 0
     
     
     elsif guess > 5
     puts "sorry, too high! input a number from 1 to 10"
     guess = gets.chomp.to_i
-    time = 0
     
     else
       puts "that's correct!"
+      time = 12
     end
     
     break
